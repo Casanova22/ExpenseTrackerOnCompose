0e449ba6aa2d2c8bff8750e1798e5a9dfa53e18a
cyril.casanova <cyril.casanova@viralpicks.ph>
2026-04-28 16:57:46 +0800

    Refactored

--- app/src/main/java/com/ceej/jc/expensetracker/PasswordValidationState.kt
+++
@@ -1,9 +0,0 @@
-package com.ceej.jc.expensetracker
-
-data class PasswordValidationState(
-    val hasUppercaseCharacter : Boolean = false,
-    val hasSpecialCharacter : Boolean = false,
-    val hasNumericalCharacter : Boolean = false,
-    val passwordsMatch : Boolean = false,
-    val passwordSuccessful : Boolean = false
-)
--- app/src/main/java/com/ceej/jc/expensetracker/UsernameValidationState.kt
+++
@@ -1,9 +0,0 @@
-package com.ceej.jc.expensetracker
-
-data class UsernameValidationState(
-    val usernameContainsLetters : Boolean = false,
-    val usernameContainsNumbers : Boolean = false,
-    val usernameContainsSpecialCharacters : Boolean = false,
-    val usernameContainsSpaces : Boolean = false,
-    val usernameSuccessful : Boolean = true
-)
