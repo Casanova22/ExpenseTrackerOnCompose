7d8ca1340c498cf5af4a6a6d65437d21c24f4cee
cyril.casanova <cyril.casanova@viralpicks.ph>
2026-07-25 01:21:24 +0800

    chore: removed generated files from VC

--- data/src/main/java/com/ceej/common/expensetracker/data/network/NetworkConnectivityObserver.kt
+++ data/src/main/java/com/ceej/common/expensetracker/data/network/NetworkConnectivityObserver.kt
@@ -1,5 +1,6 @@
 package com.ceej.common.expensetracker.data.network
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.Network
@@ -8,7 +9,6 @@ import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.launch
 
 class NetworkConnectivityObserver(
     context: Context
@@ -16,35 +16,32 @@ class NetworkConnectivityObserver(
 
     private val connectivityManager =
         context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+@SuppressLint("MissingPermissions")
+    override fun observe(): Flow<ConnectivityObserver.Status> = callbackFlow {
+        val callback = object : ConnectivityManager.NetworkCallback() {
 
-    override fun observe(): Flow<ConnectivityObserver.Status> {
-        return callbackFlow {
-            val callback = object : ConnectivityManager.NetworkCallback() {
-                override fun onAvailable(network: Network) {
-                    super.onAvailable(network)
-                    launch { send(ConnectivityObserver.Status.Available) }
-                }
-
-                override fun onLosing(network: Network, maxMsToLive: Int) {
-                    super.onLosing(network, maxMsToLive)
-                    launch { send(ConnectivityObserver.Status.Losing) }
-                }
-
-                override fun onLost(network: Network) {
-                    super.onLost(network)
-                    launch { send(ConnectivityObserver.Status.Lost) }
-                }
-
-                override fun onUnavailable() {
-                    super.onUnavailable()
-                    launch { send(ConnectivityObserver.Status.Unavailable) }
-                }
+            override fun onAvailable(network: Network) {
+                trySend(ConnectivityObserver.Status.Available)
             }
 
-            connectivityManager.registerDefaultNetworkCallback(callback)
-            awaitClose {
-                connectivityManager.unregisterNetworkCallback(callback)
+            override fun onLost(network: Network) {
+                trySend(ConnectivityObserver.Status.Lost)
             }
-        }.distinctUntilChanged()
-    }
+
+            override fun onUnavailable() {
+                trySend(ConnectivityObserver.Status.Unavailable)
+            }
+
+            override fun onLosing(network: Network, maxMsToLive: Int) {
+                trySend(ConnectivityObserver.Status.Losing)
+            }
+        }
+
+        connectivityManager.registerDefaultNetworkCallback(callback)
+
+        awaitClose {
+            connectivityManager.unregisterNetworkCallback(callback)
+        }
+    }.distinctUntilChanged()
 }
+
